dce2ab745ae08f467da9af7ec7178b36eccc4912
Eric Tran <e3tran@ucsd.edu>
2019-06-09 10:19:10 -0700

    Lesson 3

---
+++ Lesson3/hw/hw10.java
@@ -0,0 +1,23 @@
+/*
+    hw10
+
+    Your job is to print a rectangle to the terminal using nested for loops. 
+		This should be very similar to how you implemented a square. For example:
+
+    *****
+    *****
+    *****
+
+
+		should be printed if we want to print a 3x5 rectangle.
+
+		Note: if you already know what methods are, do not create any helper
+		methods to draw your objects. You can only code inside the scope of
+		the "main" function
+ */
+
+public class hw10 {
+	public static void main( String[] args ) {
+			// TODO
+	}
+}
---
+++ Lesson3/hw/hw9.java
@@ -0,0 +1,24 @@
+/*
+    hw9
+
+    Your job is to print a square to the terminal using nested for loops. For example:
+
+    *****
+    *****
+    *****
+    *****
+    *****
+
+
+		should be printed if we want to print a 5x5 square.
+
+		Note: if you already know what methods are, do not create any helper
+		methods to draw your objects. You can only code inside the scope of
+		the "main" function
+ */
+
+public class hw9 {
+	public static void main( String[] args ) {
+			// TODO
+	}
+}
