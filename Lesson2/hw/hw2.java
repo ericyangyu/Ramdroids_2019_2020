4ecf28a7a91f328a9951960bf9b31c4a756db8c8
alexa <alexa@DESKTOP-KHVR8HS>
2019-06-30 14:29:16 -0700

    Finished hw number 2

--- Lesson2/hw/hw2.java
+++ Lesson2/hw/hw2.java
@@ -22,6 +22,22 @@ public class hw2 {
 		// TODO: Create your if logic here. Assume "power" is your power level.
 		// hint: to check if power is 100-299, your logic is like how you say it out loud.
 		// You are between 100 and 299, or in other words, greater than 100 and less than 299.
-	}
+		if( power <= 100 ) {
+		    System.out.println( "pathetic" );
 }
+		else if( power>100 && power<=299 ) {
+		    System.out.println( "heckers" );
+}
+		else if( power>300 && power<=799 ) {
+			System.out.println( "getting powerful" );
+}
+		else if( power>800 && power<=2999 ) {
+			System.out.println( "oof" );
+}
+		else if( power>3000 && power<=9000 ) {
+			System.out.println( "youch" );
+}
+		else {
+		    System.out.println( "IT'S OVER 9000!!!!" );
+} } }
 
