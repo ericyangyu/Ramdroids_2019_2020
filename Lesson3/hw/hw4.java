31d02baa699debc21a55a64116ca9603c8a93ae4
annonymousprogrammer <yinduwara1653@tcusd.net>
2019-07-07 11:45:52 -0700

    Finished

--- Lesson3/hw/hw4.java
+++ Lesson3/hw/hw4.java
@@ -29,8 +29,20 @@
 	You may need to do some math!
  */
 
-public class hw11 {
+public class hw4 {
 	public static void main( String[] args ) {
-			// TODO
+		 int rows = 3, k = 0;
+	        for(int i = 1; i <= rows; ++i, k = 0) {
+	            for(int space = 1; space <= rows - i; ++space) {
+	                System.out.print("  ");
+	            }
+	            while(k != 2 * i - 1) {
+	                System.out.print("* ");
+	                ++k;
+	            }
+	            System.out.println();
+	        }
+	    }
+	
 	}
-}
+
