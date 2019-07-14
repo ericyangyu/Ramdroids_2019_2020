98f494b0161cdb1f6c9764578efdc35836e0b22c
alexa <alexa@Alexander-PC>
2019-07-14 10:52:47 -0700

    edits to hw4

--- Lesson3/hw/hw4.java
+++ Lesson3/hw/hw4.java
@@ -33,14 +33,14 @@ public class hw4 {
 	public static void main( String[] args ) {
 		int y = 0;
 		for(int x = 0; x <= 2; x++, y = 0) {
-			for(int z = 0; z <= 2 - x; z++) 
-			{	System.out.print(" ");
+			for(int z = 0; z <= 2 - x; z++) {	
+				System.out.print(" ");
 			}
-			 while(y != 2 * x + 1) 
-			 {
+			 while(y != 2 * x + 1) {
 	            System.out.print("*");
 	            	y++;
 	         }
-	    }
+			 System.out.println("");
+	    }	
 	}
 }
