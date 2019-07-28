ec339efd02d6d796144822a3bd803766a538b296
annonymousprogrammer <yinduwara1653@tcusd.net>
2019-07-27 18:02:01 -0700

    Help?

--- Lesson3/hw/hw5.java
+++ Lesson3/hw/hw5.java
@@ -36,12 +36,39 @@
 
 public class hw5 {
 	public static void main( String[] args ) {
-		while( /* TODO */ ) {
-			// TODO
-			while( /* TODO */ ) {
-				//TODO
-			}
+		int next = 10;
+		while( next == 10 ) {
+			 int lines = 3, y = 0;
+		        for(int i = 1; i <= lines; ++i, y = 0) {
+		            for(int space = 1; space <= lines - i; ++space) {
+		                System.out.print("  ");
+		            }
+		            while(y != 2 * i - 1) {
+		                System.out.print("* ");
+		                ++y;
+		            }
+		            System.out.println();
+		        }
+		        next--;
 		}
-		// you may want to consider having another set of nested while loops to draw the inverted triangle
+			while( next== 9 ) {
+				int rows = 2;
+		        for(int z = rows; z >= 1; --z) {
+		            for(int space = 1; space <= rows - z; ++space) {
+		                System.out.print("  ");
+		            }
+		            for(int j=z; j <= 2 * z - 1; ++j) {
+		                System.out.print("* ");
+		            }
+		            for(int j = 0; j < z - 1; ++j) {
+		                System.out.print("* ");
+		            }
+		            System.out.println();
+		        }
+			      next--;  }
+			    }
 	}
-}
+	
+		// you may want to consider having another set of nested while loops to draw the inverted triangle
+	
+
